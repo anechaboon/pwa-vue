22b214414494dd6729d7e31959e817a90de2b01d
anechaBoon <82567055+anechaboon@users.noreply.github.com>
2022-04-13 15:22:08 +0700

    deploy to firebase hosting

--- src/service-worker.js
+++ src/service-worker.js
@@ -8,3 +8,47 @@ self.addEventListener("message", msg=>{
         self.skipWaiting()
     }
 })
+
+
+var cacheName = 'video-storage';
+var filesToCache = [
+    'src/App.vue',
+    'public/images/jn1.png',
+    'public/images/jn2.png',
+    'public/images/jn3.png',
+    'public/images/jn4.png'
+];
+// add cache
+self.addEventListener('install', function(e) {
+    console.log('[ServiceWorker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+        console.log('[ServiceWorker] Caching app shell');
+        return cache.addAll(filesToCache);
+        })
+    );
+});
+// check version 
+self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+    caches.keys().then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+            console.log('[ServiceWorker] Removing old cache', key);
+            return caches.delete(key);
+        }
+        }));
+    })
+    );
+    return self.clients.claim();
+});
+// call cache
+self.addEventListener('fetch', function(e) {
+    console.log('[ServiceWorker] Fetch', e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
+        })
+    );
+});
