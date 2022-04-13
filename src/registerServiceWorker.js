264b71f7ea4fb0c13a44b69661a4f1be07ae310f
anechaBoon <82567055+anechaboon@users.noreply.github.com>
2022-04-13 18:28:47 +0700

    will back to set fcm messaging in main.js use step 3 instead

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -23,23 +23,3 @@ import { register } from 'register-service-worker'
     }
   })
 
-  register(`firebase-messaging-sw.js`, {
-    ready () {
-      console.log(
-        'firebase-messaging App is being served from cache by a service worker.\n' +
-        'For more details, visit https://goo.gl/AFskqB'
-      )
-    },
-    registered () {
-      console.log('firebase-messaging Service worker has been registered.')
-    },
-    cached () {
-      console.log('firebase-messaging Content has been cached for offline use.')
-    },
-    offline () {
-      console.log('firebase-messaging No internet connection found. App is running in offline mode.')
-    },
-    error (error) {
-      console.error('firebase-messaging Error during service worker registration:', error)
-    }
-  })
